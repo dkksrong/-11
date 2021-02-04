50afce8930e4b32d5ccbecbe9a634ac15cc26b61
dkk <38788624+dkksrong@users.noreply.github.com>
2021-02-04 15:40:32 +0800

    Update autoBestTrace.sh

--- autoBestTrace.sh
+++ autoBestTrace.sh
@@ -22,7 +22,7 @@ ip_list=(183.141.169.73 125.123.31.229 183.141.168.191 123.157.242.178)
 ip_addr=(电信1 电信2 电信3 联通1)
 # ip_len=${#ip_list[@]}
 
-for i in {0..4}
+for i in {0..3}
 do
 	echo ${ip_addr[$i]}
 	./besttrace -q 1 ${ip_list[$i]}
